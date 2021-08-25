3ad4424a0012a03f4c32ba34d21a497fa45e43e2
Sagarkhawse <sagar.khawse@gmail.com>
2021-08-25 20:47:52 +0530

    Add Item Dialog created

---
+++ app/src/main/java/com/laboontech/kotlinworkoutlist/other/WorkoutItemAdapter.kt
@@ -0,0 +1,50 @@
+package com.laboontech.kotlinworkoutlist.other
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.laboontech.kotlinworkoutlist.R
+import com.laboontech.kotlinworkoutlist.data.db.entities.WorkoutItem
+import com.laboontech.kotlinworkoutlist.ui.workoutlist.WorkoutViewModel
+import kotlinx.android.synthetic.main.item_workout.view.*
+
+class WorkoutItemAdapter(var items: List<WorkoutItem>, private val viewModel: WorkoutViewModel) :
+    RecyclerView.Adapter<WorkoutItemAdapter.WorkoutViewHolder>() {
+
+    inner class WorkoutViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WorkoutViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_workout, parent, false)
+        return WorkoutViewHolder(view)
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun onBindViewHolder(holder: WorkoutViewHolder, position: Int) {
+        val data = items[position]
+        holder.itemView.tvWorkoutName.text=data.name
+        holder.itemView.tvSets.text="Sets : ${data.name}"
+
+        holder.itemView.delete.setOnClickListener {
+            viewModel.delete(data)
+        }
+
+        holder.itemView.add.setOnClickListener {
+            data.sets++
+            viewModel.upsert(data)
+        }
+
+        holder.itemView.minus.setOnClickListener {
+            if (data.sets>0){
+                data.sets--
+                viewModel.upsert(data)
+            }
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+}
